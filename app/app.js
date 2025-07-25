1cb2b7b11625a0eba45b74eb4494f8e4822193b2
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2025-07-25 18:55:24 +1000

    Update app.js

--- app/app.js
+++ app/app.js
@@ -197,6 +197,13 @@ var app = new Vue({
      					<li><p> <img src="img/MicroUSB.jpg" alt="Pineapple" style="width:170px;height:60px;"> </p></li>
 				</ul>
 			    </li>
+       			    <li>
+			    	<p>Check against the firmware version name and determine if it uses Normal Mode or Bootloader Mode.</p>
+				<ul>
+					<li><p>If the buttons were pressed in the correct order the flashing LED will turn off</p></li>
+					<li><p> <img src="img/CTFW-sm.jpg" alt="Pineapple" style="width:170px;height:60px;"> </p></li>
+				</ul>
+			    </li>
                             <li>
 			    	<p>On the daughter-board PCB enable the COLORTONE Firmware Update by holding the BOOT button down(1), and then pressing, and releasing the RESET button(2).</p>
 				<ul>
