3c7da5004a148d52794f48cd85049446f2349225
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2025-07-26 08:52:18 +1000

    Update app.js

--- app/app.js
+++ app/app.js
@@ -228,10 +228,10 @@ var app = new Vue({
                             
                             <li><p>Click the Connect button at the top of this page.</p></li>
                             <li>
-			    	<p>In the PopUp Dialog window select either "DFU in FS Mode" or "Bootloader" from the list</p>
+			    	<p>In the PopUp Dialog window select either "DFU in FS Mode" or "Daisy Bootloader" from the list</p>
 				<p>TODO Show Image of example dialog window</p>
 			    	<ul>
-			    		<li><p>If "DFU in FS Mode" or "Bootloader" does not appear in the list try another Cable or USB port</p></li>
+			    		<li><p>If "DFU in FS Mode" or "Daisy Bootloader" does not appear in the list try another Cable or USB port</p></li>
 			    	</ul>
 			    </li>
 			    
