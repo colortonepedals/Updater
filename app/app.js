5adad0ca1be7f60b104778a3717c53e4e61b2d4a
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2025-07-25 19:45:51 +1000

    Update app.js

--- app/app.js
+++ app/app.js
@@ -193,7 +193,7 @@ var app = new Vue({
 			    	<p>Unscrew and remove the back cover of your COLORTONE Pedal and connect the PCB Board via a Micro USB (DShape) to your Computer</p>
 			    	<ul>
 					<li><p>A direct connection between Pedal and Computer is recommended - not via a USB hub</p></li>
-					<li><p>The orientation of the USB 'D' connector should have the flat side facing up when inserting into the USB socket</p></li>
+					<li><p>The orientation of the Micro USB 'D' connector should have the flat side facing up when inserting into the USB socket</p></li>
      					<li><p> <img src="img/MicroUSB.jpg" alt="Pineapple" style="width:170px;height:60px;"> </p></li>
 				</ul>
 			    </li>
