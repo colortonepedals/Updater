d375244184b2a21b95a1b756c7fc446def9120e2
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2025-07-25 19:08:32 +1000

    Update app.js

--- app/app.js
+++ app/app.js
@@ -198,16 +198,16 @@ var app = new Vue({
 				</ul>
 			    </li>
        			    <li>
-			    	<p>Check against the firmware version name and determine if it uses Normal Mode or Bootloader Mode.</p>
+			    	<p>Check against the firmware version name and determine if it uses NORMAL MODE or BOOTLOADER MODE.</p>
 				<ul>
 					<li><p>FOR NORMAL MODE</p></li>
-     					<li><p>On the daughter-board PCB enable the COLORTONE Firmware Update by holding the BOOT button down(1), and then pressing, and releasing the RESET button(2)</p></li>
-     					<li><p>If the buttons were pressed in the correct order the flashing LED will turn off</p></li>
+     					<li><p>On the daughter-board PCB enable the COLORTONE Firmware Update by holding the BOOT button down(1). While BOOT is Pressed then also press the RESET button(2). When both buttons have been pressed you can then release both</p></li>
+     					<li><p>If the buttons were pressed in the correct order the flashing LED on the daughter board will turn off</p></li>
 					<li><p> <img src="img/CTFW-sm.jpg" alt="Pineapple" style="width:170px;height:60px;"> </p></li>
 				</ul>
     				<ul>
 					<li><p>FOR BOOTLOADER MODE</p></li>
-     					<li><p>On the daughter-board PCB enable the COLORTONE Firmware Update by pressing the BOOT button(1) once, and then pressing the RESET button(2) once</p></li>
+     					<li><p>On the daughter-board PCB enable the COLORTONE Firmware Update by pressing and releasing the BOOT button(1) once, and then pressing and releasing the RESET button(2) once</p></li>
 					<li><p>If the buttons were pressed in the correct order the flashing LED will begin pulsing</p></li>
 					<li><p> <img src="img/CTFW-sm.jpg" alt="Pineapple" style="width:170px;height:60px;"> </p></li>
 				</ul>
