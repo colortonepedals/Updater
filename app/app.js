54cc0ae40ab33e8800d0d38a94e23ec75c4bc7e6
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2025-11-12 14:19:48 +1100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -202,9 +202,8 @@ var app = new Vue({
        					<li>
                     		<p>Select the ZeroSum PEDAL and FIRMWARE VERSION update required from the two drop down menus below on this page:</p>
                         	<ul>
-				    		<li><p> <img src="img/select2.jpg" alt="Pineapple" style="width:800px;height:198px;"> </p></li>
-                                    <li><p>Select your Colortone Pedal and Firmware Version from the drop down menu </p></li>
-                                    <li><p>If you have been sent a custom build, click the Choose File button and navigate to and select the supplied .bin file you would like to load.</p></li>
+				    			<li><p> <img src="img/select2.jpg" alt="Pineapple" style="width:800px;height:198px;"> </p></li>
+                                <li><p>NOTE: If you have been sent a custom build, click the Choose File button and navigate to and select the supplied .bin file you would like to load.</p></li>
                         	</ul>
                 		</li>
        					<li>
