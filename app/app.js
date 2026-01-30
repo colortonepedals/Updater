c528e9811be5b9de6999049006c9f4fa4c3839ee
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2026-01-31 08:12:45 +1100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -692,12 +692,12 @@ var app = new Vue({
             var blink_example = self.examples.filter(example => example.name.toLowerCase() === "blink" && example.platform === "seed")[0]
 
             // Read new file
-      //      self.firmwareFileName = blink_example.name
-      //      var srcurl = blink_example.source.repo_url
-      //      var expath = srcurl.concat(blink_example.filepath)
-      //  	readServerFirmwareFile(expath, false).then(buffer => {
-      //          blinkFirmwareFile = buffer
-      //      })
+            self.firmwareFileName = blink_example.name
+            var srcurl = blink_example.source.repo_url
+            var expath = srcurl.concat(blink_example.filepath)
+        	readServerFirmwareFile(expath, false).then(buffer => {
+                blinkFirmwareFile = buffer
+            })
 
             // grab the bootloader firmware file   //AH 2026
             var srcurl = blink_example.source.bootloader_url
