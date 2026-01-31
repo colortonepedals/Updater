10ffaa0fb8bb63ae1827998b7ee56ef5a370fe20
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2026-01-31 16:04:52 +1100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -530,6 +530,10 @@ var app = new Vue({
                                 drop-placeholder="Drop file here..."
                             ></b-form-file>
                     </b-row>
+					<b-row class="p-2">
+                        <legend> </legend>
+                        <div><b-button variant="es" id="blink"  :disabled="no_device">For Testing Only</b-button></div>
+                    </b-row>
                 </b-container>
             </b-col>
         </b-row>
