2d51b3fa9f0a9756b0e63f575ef74be60810ce1f
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2026-01-31 11:40:43 +1100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -549,7 +549,7 @@ var app = new Vue({
             <br> <br>
             <b-button variant="es" v-b-toggle.collapseAdvanced>Advanced...</b-button>
             <b-collapse id="collapseAdvanced">
-                <br> <div> <b-button variant="esf" id="bootloader"  :disabled="no_device">Flash_Bootloader_Image_</b-button> </div>                        
+                <br> <div> <b-button variant="esf" id="bootloader"  :disabled="no_device">Flash Bootloader Image</b-button> </div>                        
             </b-collapse>
 
             <div class="log" id="downloadLog"></div>            
