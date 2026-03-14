76c76db26e330fc76f6f0d9d3b7749ddaf6af7e6
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2026-03-14 15:49:21 +1100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -170,7 +170,7 @@ var app = new Vue({
 		
     	<b-row align="center" class="app_column">
         	<div>
-            	<legend>COLORTONE_FIRMWARE_UPDATER_</legend>
+            	<legend>COLORTONE_FIRMWARE_UPDATER</legend>
             	<p> Connect to the Board - If this is your first time here, follow the steps under the 'Display Help' buttons below </p>
             	<p> <img src="img/colortone_Logo_2023_2.png" alt="Pineapple" style="width:170px;height:130px;"> </p>
             	<p><b-button variant="ct" id="connect"> Connect</b-button></p>
