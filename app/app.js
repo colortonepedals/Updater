2f0ed589d38eb5259cbc90a516f131e66d7727e1
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2026-03-15 13:18:32 +1100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -173,7 +173,7 @@ var app = new Vue({
         	<div>
 				<div class="wrapper">
 					<div>
-            			<legend>COLORTONE_FIRMWARE_UPDATER</legend>
+            			<legend>COLORTONE FIRMWARE UPDATER</legend>
             			<p> Connect to the PCB Board via USB - If this is your first time here, follow the steps under the 'Display Help' buttons below </p>
             			<p> <img src="img/colortone_Logo_2023_2.png" alt="Pineapple" style="width:170px;height:130px;"> </p>
             			<p><b-button variant="ct" id="connect"> Connect</b-button></p>
