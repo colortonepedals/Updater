e22397f16300e501a0990fdf5fb33c66621d2e57
colortonepedals <130120769+colortonepedals@users.noreply.github.com>
2026-03-15 13:24:36 +1100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -227,7 +227,7 @@ var app = new Vue({
        									<li>
                     						<p>Select 'Bootloader' and 'Bootloader V6.3 Internal' from the two drop down menus below on this page:</p>
                         					<ul>
-				    							<li><p> <img src="img/BootloaderImageWeb.png" alt="Pineapple" style="width:800px;height:198px;"> </p></li>
+				    							<li><p> <img src="img/BootloaderImageWeb.png" alt="Pineapple" style="width:400px;height:99px;"> </p></li>
                         					</ul>
                 						</li>
                 						<li><p>Click the 'Connect' button at the top of this page.</p></li>
@@ -286,7 +286,7 @@ var app = new Vue({
        								<li>
                     					<p>Select the ZeroSum PEDAL and latest FIRMWARE VERSION update required from the two drop down menus below on this page:</p>
                         					<ul>
-				    							<li><p> <img src="img/ZSBoot.png" alt="Pineapple" style="width:800px;height:198px;"> </p></li>
+				    							<li><p> <img src="img/ZSBoot.png" alt="Pineapple" style="width:400px;height:99px;"> </p></li>
                                 				<li><p> NOTE: If you have been sent a custom build, click the Choose File button and navigate to and select the supplied .bin file you would like to load.</p></li>
                         					</ul>
                 					</li>
@@ -344,7 +344,7 @@ var app = new Vue({
        								<li>
                     					<p>Select the ZeroSum PEDAL and latest FIRMWARE VERSION update required from the two drop down menus below on this page:</p>
                         				<ul>
-				    						<li><p> <img src="img/SelectZS.jpg" alt="Pineapple" style="width:800px;height:198px;"> </p></li>
+				    						<li><p> <img src="img/SelectZS.jpg" alt="Pineapple" style="width:400px;height:99px;"> </p></li>
                                 			<li><p> NOTE: If you have been sent a custom build, click the Choose File button and navigate to and select the supplied .bin file you would like to load.</p></li>
                         				</ul>
                 					</li>
